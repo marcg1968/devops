e892a738c093f29c4cb0082a73669986fa7ededa
Marc Greyling <marc@cubi201.marcjan.de>
2021-02-15 17:03:15 +0100

    bf

--- wp-single-site.sh
+++ wp-single-site.sh
@@ -2,8 +2,8 @@
 
 # Script to set up a single Wordpress installation based on specified domain
 #
-URL="https://github.com/marcg1968/devops/blob/master/wp-single-site.sh"
-URL="https://github.com/marcg1968/devops/blob/dev-01/wp-single-site.sh"
+URL="https://raw.githubusercontent.com/marcg1968/devops/master/wp-single-site.sh"
+URL="https://raw.githubusercontent.com/marcg1968/devops/dev-01/wp-single-site.sh"
 # run the script by entering
 # curl -s "$URL" | bash
 # OR
